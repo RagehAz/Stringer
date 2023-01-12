99eeee7994fabceb1f59aef3485f2b2990e61092
RagehAz <rageh.az@gmail.com>
2023-01-13 01:33:29 +0200

    - added old method test

--- test/stringer_test.dart
+++ test/stringer_test.dart
@@ -1,12 +1,69 @@
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:stringer/stringer.dart';
-//
-// void main() {
-//
-//   test('adds one to input values', () {
-//
-//
-//   });
-//
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stringer/stringer.dart';
+
+void main(){
+  // -----------------------------------------------------------------------------
+
+  /// STRINGS ADDED AND REMOVED
+
+  // --------------------
+  const List<String> _oldList =            <String>['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  const List<String> _newWithAdded =   <String>['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+  const List<String> _newWithRemoved = <String>['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+  // --------------------
+  test('getAddedStrings', () {
+
+    final List<String> _added = Stringer.getAddedStrings(
+      oldStrings: _oldList,
+      newStrings: _newWithAdded,
+    );
+    expect(_added, ['k']);
+
+
+  });
+  // --------------------
+  test('getRemovedStrings', () {
+
+    final List<String> _removed = Stringer.getRemovedStrings(
+      oldStrings: _oldList,
+      newStrings: _newWithRemoved,
+    );
+    expect(_removed, ['j']);
+
+    final List<String> _removed2 = Stringer.getRemovedStrings(
+      oldStrings: [],
+      newStrings: _newWithRemoved,
+    );
+    expect(_removed2, []);
+
+    final List<String> _removed3 = Stringer.getRemovedStrings(
+      oldStrings: null,
+      newStrings: _newWithRemoved,
+    );
+    expect(_removed3, []);
+
+
+    final List<String> _removed4 = Stringer.getRemovedStrings(
+      oldStrings: [null],
+      newStrings: _newWithRemoved,
+    );
+    expect(_removed4, [null]); // indeed it should remove the null
+
+
+  });
+  // -----------------------------------------------------------------------------
+  test('removeStringsFromStrings', () async {
+
+    const List<String> _source = <String>['wa7ed', 'etneen', 'talata', 'arba3a'];
+
+    const List<String> _toRemove = <String>['wa7ed', 'etneen'];
+
+    final List<String> _modified = Stringer.removeStringsFromStrings(
+        removeFrom: _source,
+        removeThis: _toRemove,
+    );
+
+    expect(_modified, <String>['talata', 'arba3a']);
+  });
+  // -----------------------------------------------------------------------------
+}
